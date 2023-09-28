4b846513024794135c246dc72bb1fbdc8f44da42
gustavosuisso <0123guga@gmail.com>
2023-09-28 19:08:42 -0300

    javadoc e palavras reservas

---
+++ java-anatomia-classes/src/edu/gustavo/javadoc/Comentarios.java
@@ -0,0 +1,28 @@
+package edu.gustavo.javadoc;
+
+public class Comentarios {
+    
+    public static void main(String[] args){
+
+        // Comentário de uma única linha.
+
+        /* 
+         * Comentários mais detalhado
+         * em multiplas linhas
+         */
+
+       
+
+    }
+
+      /**
+          * comentarios com dois asteriscos é para identificar que queremos
+          * elaborar um comentário a nivel de documentação
+          * fora do bloco do método
+          */
+          public void metodo(){
+
+            
+
+          }
+}
